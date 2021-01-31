3e4b3a05fb0b48ce06eb0085b408433e8e3976cf
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2021-01-31 16:44:49 +0700

    nav_3 (done 98%)

---
+++ script/main.js
@@ -0,0 +1,6 @@
+function load_login(){
+
+}
+function load_signin(){
+    
+}
--- script/vue-components.js
+++ script/vue-components.js
@@ -9,6 +9,14 @@ Vue.component('nav-bar',{
 let app = new Vue({
     el:"#main",
     data: {
+        // user id = 0 (not logged in yet)(1 as admin)
+        user_id=0,
         
+    },
+    method: {
+        main: load_main(),
+        login: load_login(),
+        signin: load_signin(),
+
     }
 })
