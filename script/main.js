d8496661c07a818fa1b62c7904e1b92f5c3a6a38
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2021-01-31 17:34:29 +0700

    nav_up

--- script/main.js
+++ script/main.js
@@ -1,6 +1,1 @@
-function load_login(){
 
-}
-function load_signin(){
-    
-}
--- script/vue-components.js
+++ script/vue-components.js
@@ -10,13 +10,27 @@ let app = new Vue({
     el:"#main",
     data: {
         // user id = 0 (not logged in yet)(1 as admin)
-        user_id=0,
-        
+        user_id:0,
+        searchInp: '',
+
     },
     method: {
-        main: load_main(),
-        login: load_login(),
-        signin: load_signin(),
+        // main: function(){
 
+        // },
+        // login: function(){
+        //     alert(1)
+        // },
+        // signin: function(){
+        //     alert(2)
+        // }
+        // ,
+        // searchMG: function(searchInp){
+        //     //str = manga name
+        //     alert(str)
+        // },
+        // fav: function(){
+        //     alert(4)
+        // },
     }
 })
