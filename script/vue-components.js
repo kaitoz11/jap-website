7cc5f5bfa921d25af947696d0e542143aa4a16ae
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2021-02-04 23:52:38 +0700

    finished_nav

--- script/vue-components.js
+++ script/vue-components.js
@@ -1,7 +1,6 @@
-Vue.component('nav-bar',{
-    template:`
-
-    `
+Vue.component('drop-bar',{
+    props: ['dropdown'],
+    template:'<li class="ho" onclick="{dropdown.func}">{{dropdown.title}}</li>'
 })
 
 
@@ -10,7 +9,7 @@ let app = new Vue({
     el:"#main",
     data: {
         // user id = 0 (not logged in yet)(1 as admin)
-        user_id:0,
+        user_id: 0,
         searchInp: '',
         users: [{
             username: "admin",
@@ -21,8 +20,11 @@ let app = new Vue({
         dropped_down: false,
         //rendering dropdown mene
         facilities: [{
-            func: "",
-            title: ""
+            func: "alert('hello')",
+            title: "say hello"
+        },{
+            func: "alert('hi')",
+            title: "say hi"
         }]
     },
     methods: {
@@ -46,6 +48,13 @@ let app = new Vue({
         },
         dropmenu: function(){
             this.dropped_down = !this.dropped_down
+        },
+        //menu
+        Profile: function(){
+
+        },
+        log_out: function(){
+            this.user_id = 0
         }
 
     }
