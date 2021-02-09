eda26b06023d344212d393a35e9c98a00dad50ed
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2021-02-09 15:20:45 +0700

    data model

---
+++ script/main.js
@@ -0,0 +1,22 @@
+let authors = [
+    {
+        name: '',
+
+    }
+]
+
+
+let mangadb = [
+    {
+        titles: [], // array of titles, [0] must be romanized jap
+        author: authors[0].name,
+        genre: [],
+        statu: "", // on going - complete
+        progress: {
+            vol: 0,
+            chap: 0,
+        }, // chap , vol
+        description: "",
+        year: 0, // published year
+    }
+];
